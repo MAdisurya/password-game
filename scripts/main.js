f861bb1a1944f0a632df27836ca0c262811ba429
Mario Adisurya <>
2019-09-30 21:10:02 +1300

    Added display changes, so team setup is displayed when clicking on play button
    * main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,20 @@ class App extends React.Component
                 <section id="pg-menu">
                     <h1 className="main-title">PASSWORD</h1>
 
-                    <PGButton buttonName="Play" className="play-btn" />
+                    <PGButton 
+                        buttonName="Play" 
+                        className="play-btn"
+                        onClick={() => {
+                            // Need to replace this with CSS / Less when implemented
+                            document.getElementById('pg-menu').style.display = 'none';
+                            document.getElementById('pg-team-setup').style.display = 'block';
+                        }} 
+                    />
                     <PGButton buttonName="Rules" className="rules-btn" />
                     <PGButton buttonName="Credits" className="credits-btn" />
                 </section>
 
-                <section id="pg-team-setup">
+                <section id="pg-team-setup" style={{display: 'none'}}>
                     <div className="team-one-setup-ctn">
                         <PGTeamSetup teamName="TEAM ONE" />
                     </div>
