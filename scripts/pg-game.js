20007e702db742d12ecf2d1235a72dae1bc82e99
Mario Adisurya <mario.adisurya@gmail.com>
2019-10-15 12:43:11 +1300

    * pg-game.js + PGGame.setRandomPassword()

--- scripts/pg-game.js
+++ scripts/pg-game.js
@@ -4,11 +4,32 @@ class PGGame extends React.Component
     {
         super(props);
 
+        this.passwordList = new PGPasswordList();
+
         this.state = {
             roundCounter: 0,
             password: "",
             currentTeamTurn: ""
         };
+    }
+
+    componentDidMount()
+    {
+        this.setRandomPassword();
+    }
+
+    /**
+     * Generates a random password from the passwordList
+     * and assigns it to state.password
+     */
+    setRandomPassword()
+    {
+        var randomNumber = 
+            Math.floor(Math.random() * (this.passwordList.list.length - 1));
+
+        this.setState({
+            password: this.passwordList.list[randomNumber]
+        });
     }
 
     render()
@@ -17,9 +38,9 @@ class PGGame extends React.Component
             <div className="game-ctn">
                 <div className="header-ctn"></div>
                 <div className="sub-header-ctn"></div>
+                <div className="password-holder">{this.state.password}</div>
                 <PGButtonCircle buttonName="Yes" className="game-btn yes-btn" />
                 <PGButtonCircle buttonName="No" className="game-btn no-btn" />
-                <div className="password-holder"></div>
             </div>
         );
     }
