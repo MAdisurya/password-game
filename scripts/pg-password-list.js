960e4ec352ca8c8bd528ac8990bb244208d79f7c
Mario Adisurya <mario.adisurya@gmail.com>
2019-10-15 11:53:37 +1300

    + pg-password-list.js + PGPasswordList
    Added initial password list for game

---
+++ scripts/pg-password-list.js
@@ -0,0 +1,19 @@
+class PGPasswordList
+{
+    constructor()
+    {
+        this.list = [
+            "horse", "door", "song", "trip", "backbone", 
+            "treasure", "garbage", "ski", "baseball",
+            "salt and pepper", "queen", "palace",
+            "computer", "hot dog", "iPad", "battery",
+            "password", "teapot", "electricity", "lightsaber",
+            "theif", "half", "toast", "lightbulb",
+            "music", "bowtie", "America", "outside", "nature",
+            "photograph", "hockey", "skate", "cowboy",
+            "roller blading", "whisk", "park", "bomb",
+            "gingerbread man", "spare", "key", "pinwheel",
+            "frog", "whistle"
+        ];
+    }
+}
