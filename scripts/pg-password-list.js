0a0f4b259e5c70323321150779056bff88cf5c96
Mario Adisurya <>
2020-01-01 22:13:14 +1300

    * pg-password-list.js
    * PGPasswordList.list from array [String] to dictionary {String: [String]}
    * Added PGPasswordList.list["ENG"] (English words) & PGPasswordList.list["INDO"] (Indonesian words)

--- scripts/pg-password-list.js
+++ scripts/pg-password-list.js
@@ -2,18 +2,42 @@ class PGPasswordList
 {
     constructor()
     {
-        this.list = [
-            "HORSE", "DOOR", "SONG", "TRIP", "BACKBONE", 
-            "TREASURE", "GARBAGE", "SKI", "BASEBALL",
-            "SALT AND PEPPER", "QUEEN", "PALACE",
-            "COMPUTER", "HOT DOG", "IPAD", "BATTERY",
-            "PASSWORD", "TEAPOT", "ELECTRICITY", "LIGHTSABER",
-            "THEIF", "HALF", "TOAST", "LIGHTBULB",
-            "MUSIC", "BOWTIE", "AMERICA", "OUTSIDE", "NATURE",
-            "PHOTOGRAPH", "HOCKEY", "SKATE", "COWBOY",
-            "ROLLER BLADING", "WHISK", "PARK", "BOMB",
-            "GINGERBREAD MAN", "SPARE", "KEY", "PINWHEEL",
-            "FROG", "WHISTLE"
-        ];
+        this.list = 
+        {
+            "ENG" : [
+                "HORSE", "DOOR", "SONG", "TRIP", "BACKBONE", 
+                "TREASURE", "GARBAGE", "SKI", "BASEBALL",
+                "SALT AND PEPPER", "QUEEN", "PALACE",
+                "COMPUTER", "HOT DOG", "IPAD", "BATTERY",
+                "PASSWORD", "TEAPOT", "ELECTRICITY", "LIGHTSABER",
+                "THEIF", "HALF", "TOAST", "LIGHTBULB",
+                "MUSIC", "BOWTIE", "AMERICA", "OUTSIDE", "NATURE",
+                "PHOTOGRAPH", "HOCKEY", "SKATE", "COWBOY",
+                "ROLLER BLADING", "WHISK", "PARK", "BOMB",
+                "GINGERBREAD MAN", "SPARE", "KEY", "PINWHEEL",
+                "FROG", "WHISTLE", "SAILBOAT", "PINEAPPLE",
+                "POPSICLE", "SPRINKLER", "KNEE", "GATE",
+                "DOGHOUSE", "SNOWBALL", "FACE", "DOORMAT", "HOOK",
+                "PEACH", "NEWSPAPER", "SCHOOL", "BEEHIVE",
+                "STOMACH", "BEAVER", "BEACH", "FRENCH FRIES",
+                "FLAGPOLE", "ARTIST", "HAIR DRYER", "GARDEN",
+                "CHALK", "CHIN", "CLAM", "FUR", "SNOWFLAKE",
+                "STORM", "COCONUT", "SILVERWARE", "LOBSTER",
+                "BOTTLE", "SEAHORSE", "HAMBURGER"
+            ],
+            "INDO" : [
+                "KARTU", "GARPU", "MOBIL", "KACAMATA", "DOMPET",
+                "BINTANG", "KASUR", "PINTU", "KURSI", "DOMBA",
+                "JENDELA", "BUNGA", "PASIR", "ANJING", "MONYET",
+                "HANDUK", "BAKSO", "SOTO", "RENDANG", "RAMBUT",
+                "BUKU", "JEMBATAN", "KAYU", "GEREJA", "NASI",
+                "AYAM GORENG", "INDO MIE", "HIDUNG", "BOLA",
+                "CELANA", "LILIN", "SEPATU", "JAKET", "KELUAR",
+                "GAJAH", "PANTAI", "KENTANG", "DUIT", "KOPI",
+                "AYUNAN", "KOTAK", "HADIA", "SURGA", "RUMPUT",
+                "ROTI", "KACANG", "NASI GORENG", "BAKPAO",
+                "ANDUK", "SATAY", "TELOR"
+            ]
+        };
     }
 }
